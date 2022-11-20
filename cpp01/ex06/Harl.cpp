962762a5bf25378fe0fd79b06c4a7d41778bd0f2
araiva <kizzaraiva@gmail.com>
2022-11-20 18:18:24 +0700

    use switch

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -2,22 +2,28 @@
 
 Harl::Harl() {}
 
-std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 void	Harl::complainFilter( std::string level ) {
 	int	startLevel = getLevel(level);
 
-	if (startLevel < 0)
+	switch (startLevel)
 	{
-		std::cerr << RED << "[ NO this log level ]" << RESET << std::endl;
-		return ;
-	}
-	for (int i = startLevel; i < maxLevel; i++) {
-		std::cout << "[" << levels[i] << "]" << std::endl;
-		fnc[i]();
+		case 0:
+			Harl::_debug();
+		case 1:
+			Harl::_info();
+		case 2:
+			Harl::_warning();
+		case 3:
+			Harl::_error();
+			break;
+		default:
+			std::cerr << RED << "[ NO this log level ]" << RESET << std::endl;
 	}
 }
 
 int	Harl::getLevel( std::string level ) {
+	std::string	levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
 	for (int i = 0; i < 4; i++) {
 		if ( Harl::toUppercase(level).compare(levels[i]) == 0 )
 			return i;
@@ -34,24 +40,22 @@ std::string	Harl::toUppercase( std::string str ) {
 
 void	Harl::_debug ( void ) {
 	std::string msg = "0 > love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!";
-	std::cout << msg << std::endl;
+	std::cout << "[DEBUG]\n" << msg << "\n" << std::endl;
 }
 
 void	Harl::_info ( void ) {
 	std::string msg = "1 > I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!";
-	std::cout << msg << std::endl;
+	std::cout << "[INFO]\n" << msg << "\n" << std::endl;
 }
 
 void	Harl::_warning ( void ) {
 	std::string msg = "2 > I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month";
-	std::cout << msg << std::endl;
+	std::cout << "[WARNING]\n" << msg << "\n" << std::endl;
 }
 
 void	Harl::_error ( void ) {
 	std::string msg = "3 > This is unacceptable! I want to speak to the manager now";
-	std::cout << msg << std::endl;
+	std::cout << "[ERROR]\n" << msg << "\n" << std::endl;
 }
 
-int			Harl::maxLevel		 = 4;
-void		(*Harl::fnc[])(void) = {_debug, _info, _warning, _error};
-std::string	Harl::levels[]		 = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
--- cpp01/ex06/Harl.hpp
+++ cpp01/ex06/Harl.hpp
@@ -14,7 +14,6 @@ public:
 
 	Harl( void );
 	static int				getLevel( std::string level );
-	static std::string*		getLevels( void );
 	static void 			complainFilter( std::string level );
 	static std::string		toUppercase( std::string str );
 
@@ -25,9 +24,6 @@ private:
 	static void	_warning ( void );
 	static void	_error ( void );
 
-	static int			maxLevel;
-	static void			(*fnc[])(void);
-	static std::string	levels[];
 }; 
 
 #endif
