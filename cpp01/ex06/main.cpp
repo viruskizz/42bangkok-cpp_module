e322f0b5b95a47ca5eec4d9e830aae1417054505
araiva <kizzaraiva@gmail.com>
2022-11-20 18:32:45 +0700

    add error

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -1,7 +1,9 @@
 #include "Harl.hpp"
 
 int main(int argc, char *argv[]) {
-	if (argc > 1)
-		Harl::complainFilter(argv[1]);
+	if (argc == 1) {
+		return (std::cerr << RED << "need filter level argument" << RESET << std::endl, 1);
+	}
+	Harl::complainFilter(argv[1]);
 	return 0;
 }
